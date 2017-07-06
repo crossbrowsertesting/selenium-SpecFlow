bcbffdc9f045649991b3276e4c37a53be1b243e5
Moo-CBT <stephenm@crossbrowsertesting.com>
2017-07-06 12:46:04 -0500

    Add files via upload
    example files.

---
+++ CBTFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: CBTFeature
+
+Scenario: Search for SpecFlow on Google
+	Given I navigate to the page "https://www.google.com"
+	And I see the page is loaded
+	When I enter Search Keyword in the Search Text box
+	And I click on Search Button
+	Then Search items shows the items related to SpecFlow
+
---
+++ CBTFeatureSteps.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace CBT_Example_2
+{
+    [Binding]
+    public class CBTFeatureSteps
+    {
+        protected RemoteWebDriver driver;
+
+        // Your username and authkey here
+        public string username = "your username";
+        public string authkey = "your authkey";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+
+
+        [Given(@"I navigate to the page ""(.*)""")]
+        public void GivenINavigateToThePage(string p0)
+        {
+            caps.SetCapability("name", "SpecFlow-CBT");
+            caps.SetCapability("record_video", "true");
+            caps.SetCapability("build", "1.35");
+            caps.SetCapability("os_api_name", "Win10-E14");
+            caps.SetCapability("browser_api_name", "FF53x64");
+            caps.SetCapability("screen_resolution", "1366x768");
+            caps.SetCapability("record_video", "true");
+            caps.SetCapability("record_network", "true");
+
+            caps.SetCapability("username", username);
+            caps.SetCapability("password", authkey);
+
+            // Start the remote webdriver
+            driver = new RemoteWebDriver(new Uri("http://hub.crossbrowsertesting.com:80/wd/hub"), caps, TimeSpan.FromSeconds(180));
+
+            // Navigate to site
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(p0);
+        }
+
+        [Given(@"I see the page is loaded")]
+        public void GivenISeeThePageIsLoaded()
+        {
+            Assert.AreEqual("Google", driver.Title);
+        }
+
+        [When(@"I enter Search Keyword in the Search Text box")]
+        public void WhenIEnterSearchKeywordInTheSearchTextBox()
+        {
+            string searchText = "Specflow";
+            driver.FindElement(By.Name("q")).SendKeys(searchText);
+        }
+
+        [When(@"I click on Search Button")]
+        public void WhenIClickOnSearchButton()
+        {
+            driver.FindElement(By.Name("btnG")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+        [Then(@"Search items shows the items related to SpecFlow")]
+        public void ThenSearchItemsShowsTheItemsRelatedToSpecFlow()
+        {
+            Assert.AreEqual("SpecFlow - Binding Business Requirements to .NET Code", driver.FindElement(By.XPath("//h3/a")).Text);
+            driver.Quit();
+        }
+    }
+}
